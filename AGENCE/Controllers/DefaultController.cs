c1b1bbcfac04fa9f8e1e96be240438c4566a93b7
zrkl <loutfy.elmehdi@ofppt-edu.ma>
2022-06-06 18:05:59 +0100

    edit and add confirmation popup

--- AGENCE/Controllers/DefaultController.cs
+++ AGENCE/Controllers/DefaultController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
@@ -61,8 +62,60 @@ namespace AGENCE.Controllers
             ViewBag.destination = new SelectList(db.villes, "id", "ville_name",tr.destination);
             return RedirectToAction("Travel");
         }
+
+        public async Task<ActionResult> Edit(int? id)
+        {
+            
+            Session["identity"] = id.Value;
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            travel TRAVEL = await db.travels.FindAsync(id);
+            if (TRAVEL == null)
+            {
+                return HttpNotFound();
+            }
+            int ID = (Int32)Session["id"];
+            if (TRAVEL.id_agency != ID)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.beginning = new SelectList(db.villes, "id", "ville_name", TRAVEL.beginning);
+            ViewBag.destination = new SelectList(db.villes, "id", "ville_name", TRAVEL.destination);
+            return View(TRAVEL);
+        }
+
         
-        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "id,id_agency,beginning,destination,date_start,date_arrive")]
+        travel TRAVEL)
+        {
+            int ID = (Int32)Session["id"];
+            int identity = (Int32)Session["identity"];
+            if (ModelState.IsValid)
+            {
+                R.DAL.Context.travel T = new R.DAL.Context.travel()
+                {
+                    id=identity,
+                    id_agency = ID,
+                    beginning = TRAVEL.beginning,
+                    destination = TRAVEL.destination,
+                    date_start = TRAVEL.date_start,
+                    date_arrive = TRAVEL.date_arrive,
+                    
+
+                };
+                db.Entry(T).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Travel");
+            }
+            ViewBag.beginning = new SelectList(db.villes, "id", "ville_name", TRAVEL.beginning);
+            ViewBag.destination = new SelectList(db.villes, "id", "ville_name", TRAVEL.destination);
+            return View(TRAVEL);
+        }
+
         public async Task<ActionResult> Delete(int? id)
         {
             if (id == null)
